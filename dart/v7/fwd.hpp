3e747430a7bacee3e05a560af9a56c2f40223ff8
Jeongseok Lee <jslee02@gmail.com>
2024-03-20 12:54:14 -0700

    [v7] Create v7 target

--- dart/v7/fwd.hpp
+++ dart/v7/fwd.hpp
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2011-2024, The DART development contributors
+ * Copyright (c) The DART development contributors
  * All rights reserved.
  *
  * The list of contributors can be found at:
@@ -32,10 +32,15 @@
 
 #pragma once
 
-#include <dart/v7/uri.hpp>
+#include <dart/config.hpp>
 
-namespace dart::common {
+namespace dart::v7 {
 
-using Uri = v7::Uri;
+struct StepInfo;
 
-} // namespace dart::common
+class Context;
+class World;
+
+class RigidBody;
+
+} // namespace dart::v7
